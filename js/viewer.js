ae74e96b0cd0c0e7b7e9fbffff266bb1eb433886
Pascal Boutin <pboutin@pilogiq.com>
2015-07-06 23:15:14 -0400

    added viewers count within the viewer

--- js/viewer.js
+++ js/viewer.js
@@ -1,16 +1,23 @@
 window.config = {
     apiStreamEmbed: 'http://www.twitch.tv/{{stream}}/embed',
-    apiChatEmbed: 'http://www.twitch.tv/{{channel}}/chat'
-}
+    apiChatEmbed: 'http://www.twitch.tv/{{channel}}/chat',
+    apiStream: 'https://api.twitch.tv/kraken/streams/{{stream}}?callback=?',
+    stream: ''
+};
+
 
 $(function() {
     var stream = /s=(\w+)$/.exec(window.location.href)[1];
+    window.config.stream = stream;
 
     $('._stream').attr('src', config.apiStreamEmbed.replace('{{stream}}', stream));
     $('._chat').attr('src', config.apiChatEmbed.replace('{{channel}}', stream));
 
     resizeFrames();
     $(window).resize(resizeFrames);
+
+    setInterval(refreshDetails, 60000);
+    refreshDetails();
 });
 
 function resizeFrames() {
@@ -20,6 +27,15 @@ function resizeFrames() {
     var streamHeight = width / 16 * 9 + 20;
     $('._stream').attr('height', streamHeight);
 
-    var chatHeight = height - streamHeight;
+    $('._viewers-container').css('top', streamHeight + 35);
+
+    var chatHeight = height - streamHeight - 25;
     $('._chat').attr('height', chatHeight);
+}
+
+function refreshDetails() {
+    var streamUrl = config.apiStream.replace('{{stream}}', window.config.stream);
+    $.getJSON(streamUrl, function(data) {
+        $('._viewers').text(data.stream.viewers);
+    });
 }
